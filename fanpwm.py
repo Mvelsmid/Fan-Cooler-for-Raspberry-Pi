efb1d3b7e3ccc99429f64bcdc9dd28e7d9b010a6
Mvelsmid <47298731+Mvelsmid@users.noreply.github.com>
2020-07-21 13:51:25 +0200

    version 1.2
    Optimalizer for RPi4

--- fanpwm.py
+++ fanpwm.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python
-# Autor M. Velsmid
-# version: 1.1
+# Autor Kiker
+# verzia 1.2
 
 import time
 import sys
 sys.path.append('/storage/.kodi/addons/virtual.rpi-tools/lib')
 import RPi.GPIO as GPIO
 
-Teplotamin = 46  # setup min. temerature
-pin = 4          # set pin BCM
-cakaj = 10       # repeat control temerature
-Hz = 30          # frekquency PWM
-advanc = 0       # 0 off - 1 on  /decrease of temperature to min.
-R = 3            # difference min. temepratre from cooling down  /it must be turned on advanc   
-RT = 120          # time in seconds to re-compare the temperature
+Teplotamin=50  #minimalna nastavena teplota /ventilator nepojde ak bude nizsia
+pin = 4        #nastavenie pinu BCM
+cakaj = 14      #nastavenie opakovania kontroly teploty
+Hz = 35        #frekvencia PWM
+advanc = 1     # 0 vypnute - 1 zapnute  /znizenie teploty na minimalnu
+R = 3          # rozdiel v minimalne nastavenej teplote od vychladenia /ak je zapnute advanc   
+RT = 30        # cas v sekundach na porovnanie teploty
 
-# config
+# konfiguracia 
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
 
@@ -27,28 +28,27 @@ s=0.0
 s2=0.0
 T1=Teplotamin
 
-# fan control
+# riadenie ventilatora
+
 try:
     while 1:
         tempfile=open("/sys/class/thermal/thermal_zone0/temp","r")
-        #tempfile=open("temp","r")
         ctemp=float(tempfile.read())/1000
         tempfile.close()
-        #T=float(Teplotamin*0.01)
         T=Teplotamin*0.01
         temp=ctemp*100
-        
-        #s=float(ctemp/100-T)
         s=ctemp/100-T
         speed=ctemp/100+s+s2
         fspeed=speed*100
+        if(fspeed > 100):      
+                   fspeed=100 
         
         if(i >= 1):
             ctempold=ctemp
             T1=Teplotamin-R
-            #print('fan on',time.ctime())
         
-        i=i+1   
+        i=i+1 
+          
         if(advanc < 1):
                 T1=Teplotamin
         
@@ -57,15 +57,7 @@ try:
             s2=0.0
             i=0
             T1=Teplotamin
-            #print('fan off',time.ctime())
-            
-        #print('ctemp:',ctemp)
-        #print('ctempold:',ctempold)
-        #print('T1:',T1)
-        #print('fspeed:',fspeed)
-        #print('s:',s)
-        #print('i:',i)
-       
+                        
         T2=i*cakaj
         
         if(T2 >= RT):
@@ -73,29 +65,27 @@ try:
                 if(ctempold <= ctemp):
                         s2=s2+0.05
                         ctempold=ctemp
-                       # print(time.ctime(),'speed+', s+s2)
+                        
                         if(ctemp/100+s+s2 > 1): 
                             s2=s2-0.05  
-                           # print(time.ctime(),'speed-', s+s2)  
+                            
                 elif(ctempold - ctemp < 1):
                         s2=s2+0.05
                         ctempold=ctemp
-                       # print(time.ctime(),'speed+', s+s2)
+                        
                         if(ctemp/100+s+s2 > 1): 
                             s2=s2-0.05  
-                           # print(time.ctime(),'speed-', s+s2) 
-  
+                            
         else:
-                s=0.0  
+                s=0.0    
             
-        if(fspeed > 100):
-                fspeed=100
+    
         fan.ChangeDutyCycle(fspeed)
         time.sleep(cakaj)
 
 
 except(KeyboardInterrupt):
-    print('break CTRL+C')
+    print('prerusenie CTRL+C')
     GPIO.cleanup()
     sys.exit()
     
